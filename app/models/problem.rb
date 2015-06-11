620109e8baa43094afcc02d7d37af4268483cd0e
ericaporter <ericaporter001@gmail.com>
2015-06-11 21:06:13 +0100

    added validations

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -7,5 +7,11 @@ class Problem < ActiveRecord::Base
   has_many :solutions
   has_many :votes, through: :solutions
   accepts_nested_attributes_for :solutions, :allow_destroy => true, :reject_if => proc { |a| a['title'].blank? }
-  validates :category, presence: true
+  validates_presence_of :category_id, message: "You must select a category."
+  validate :have_solutions
+  def have_solutions
+    if solutions.size < 2
+      errors.add(:base, "Must have at least 2 options.")
+    end
+  end
 end
