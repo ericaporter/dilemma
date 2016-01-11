b421b1df41f65bafd38f62e1e0ccacfa8329e62f
ericaporter <ericaporter001@gmail.com>
2016-01-11 11:02:27 +0000

    Convert problem partial to slim and change nav font
    -Remove nav styling from application file
    -Change font for nav
    -Remove label from login form and add placeholders
    -Convert problem partial to slim
    -Update success colour
    -Update secondary button colour

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+#seed with 4x users
+harry = User.new(name: 'Harry', email: 'harry@email.com', password: 'password', password_confirmation: 'password')
+harry.problems.create(content:'This is a problem')
 
-#create a different profile pic for each user
+
+
+
 user = User.create!([
     { name: 'Harry', email: 'harry@email.com', password: 'password', password_confirmation: 'password', profile_image:'' },
     { name: 'Dan', email: 'dan@email.com',  password: 'password',  password_confirmation: 'password' },
@@ -11,10 +16,7 @@ user = User.create!([
     ])
 
 #create a sample problems for each user - how to give it a category?
-problem = User.create(content:'This is a problem')
-
-#create a different background image for each problem
-
+problem = Problem.create(content:'This is a problem', user_id: 12)
 
 #create a category for each dilemma
 categories = ['Love', 'Life', 'Relationships', 'Money', 'Work', 'Study', 'Travel', 'Shopping']
